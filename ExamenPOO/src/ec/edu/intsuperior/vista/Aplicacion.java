52321c2ac6fa02d9ea7c2c90f8691d9567f12362
ivanquishpeint <86636767+ivanquishpeint@users.noreply.github.com>
2021-08-18 22:28:10 -0500

    Add files via upload
    QUISHPE_IVAN_EXAMEN_POO_2A

---
+++ ExamenPOO/src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.vista;
+
+import ec.edu.intsuperior.modelo.Cliente;
+import ec.edu.intsuperior.modelo.Persona;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Aplicacion {
+
+    public static void main(String[] args) {
+
+        Persona p1 = new Persona();
+        Persona p2 = new Persona("Ivan Quishpe","21");
+        Cliente c1 = new Cliente();
+        Cliente c2 = new Cliente("0959048035");
+
+        System.out.println("El clinte: " + p2.getNombre());
+        System.out.println("De: " + p2.getEdad() + " Años de Edad");
+        System.out.println("Con el numero de telefono: " + c2.getTelefono());
+    }
+
+}
